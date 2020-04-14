e4f04cadc648b8a787a231bf86ff7fdeb6f29566
Jure Glasovic <jglasovic@gmail.com>
2020-04-14 22:26:58 +0200

    test migrations

--- scripts/clean-database.ts
+++ scripts/clean-database.ts
@@ -2,10 +2,26 @@ require('dotenv').config();
 import { Config as BaseConfig } from '../src/core/utils';
 import { Client } from 'pg';
 
+const query = `
+do
+$$
+declare
+  l_stmt text;
+begin
+  select 'truncate ' || string_agg(format('%I.%I', schemaname, tablename), ',')
+    into l_stmt
+  from pg_tables
+  where schemaname in ('public');
+
+  execute l_stmt;
+end;
+$$
+`;
+
 (async () => {
   try {
     const Config = new BaseConfig();
-    const connectionString = Config.getDatabaseConnectionString('postgres');
+    const connectionString = Config.getDatabaseConnectionString();
     const client = new Client({
       connectionString,
       ssl: Config.getString('NODE_TLS_REJECT_UNAUTHORIZED') === '0',
@@ -14,13 +30,8 @@ import { Client } from 'pg';
     console.log(`Connecting to database: ${databaseName}`);
     await client.connect();
     console.log('Postgres client connected.');
-    const result = await client.query(`SELECT 1 FROM pg_database WHERE datname='${databaseName}'`);
-    if (result.rows.length === 0) {
-      await client.query(`CREATE DATABASE ${databaseName} ENCODING 'UTF8';`);
-      console.info(`Database created!`);
-    } else {
-      console.log('Database already exists, skipping...');
-    }
+    await client.query(query);
+    console.info(`Database cleaned!`);
     await client.end();
   } catch (err) {
     console.error(`Could not create database: ${err}`);
